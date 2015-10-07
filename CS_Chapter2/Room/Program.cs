d34c3ab46150d1da3f5f007d03f6f00a95695cb7
shookt <tshook428@gmail.com>
2015-10-07 11:23:51 -0400

    second commit

--- CS_Chapter2/Room/Program.cs
+++ CS_Chapter2/Room/Program.cs
@@ -9,7 +9,12 @@ namespace Room
     {
         static void Main(string[] args)
         {
-            
+            double width = 25;
+            Console.WriteLine("The width of the room is {0} feet", width);
+            double length = 30;
+            Console.WriteLine("The length of the room is {0} feet", length);
+            double area = length*width;
+            Console.WriteLine("The area of the room is {0} square feet", area);
         }
     }
 }
--- CS_Chapter2/Yards/Program.cs
+++ CS_Chapter2/Yards/Program.cs
@@ -9,6 +9,15 @@ namespace Yards
     {
         static void Main(string[] args)
         {
+            double width = 42;
+            Console.WriteLine("The width of the room is {0} feet", width);
+            double length = 25;
+            Console.WriteLine("The length of the room is {0} feet", length);
+            double area = length * width;
+            Console.WriteLine("The area of the room is {0} square feet", area);
+            Console.WriteLine("The cost of carpeting is $1.53 per square foot.");
+            double price = area / 9 * 1.53;
+            Console.WriteLine("The total cost of your carpeting is {0}", price.ToString("C"));
         }
     }
 }
